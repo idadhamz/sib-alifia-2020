82b3c2eb4e21daf575171fad978191d540723392
Dadi Ilham <dadiilhamsetiadii@gmail.com>
2019-12-02 10:23:35 +0700

    dad

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,8 +14,10 @@
         <link rel="shortcut icon" href="{{ asset('assets/img/p-50.png') }}">
 
         <!-- General CSS Files -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+        <!-- <link rel="stylesheet" href="{{asset('assets/css/all.css')}}"> -->
 
         <!-- Template CSS -->
         <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
@@ -70,22 +72,27 @@
 
 
         <!-- jQuery  -->
-        <script src="{{asset('admin/assets/js/jquery.min.js')}}"></script>
-        <script src="{{asset('admin/assets/js/bootstrap.min.js')}}"></script>
-        <script src="{{asset('admin/assets/js/detect.js')}}"></script>
+        <!-- <script src="{{asset('admin/assets/js/jquery.min.js')}}"></script> -->
+        <!-- <script src="{{asset('admin/assets/js/bootstrap.min.js')}}"></script> -->
+        <!-- <script src="{{asset('admin/assets/js/detect.js')}}"></script>
         <script src="{{asset('admin/assets/js/fastclick.js')}}"></script>
         <script src="{{asset('admin/assets/js/jquery.blockUI.js')}}"></script>
         <script src="{{asset('admin/assets/js/waves.js')}}"></script>
-        <script src="{{asset('admin/assets/js/wow.min.js')}}"></script>
-        <script src="{{asset('admin/assets/js/jquery.nicescroll.js')}}"></script>
-        <script src="{{asset('admin/assets/js/jquery.scrollTo.min.js')}}"></script>
+        <script src="{{asset('admin/assets/js/wow.min.js')}}"></script> -->
+        <!-- <script src="{{asset('admin/assets/js/jquery.nicescroll.js')}}"></script> -->
+        <!-- <script src="{{asset('admin/assets/js/jquery.scrollTo.min.js')}}"></script> -->
 
         <!-- General JS Scripts -->
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script> -->
+        <script src="{{asset('assets/js/jquery-3.3.1.min.js')}}"></script>
+        <script src="{{asset('assets/js/popper.min.js')}}"></script>
+        <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('assets/js/jquery.nicescroll.min.js')}}"></script>
+        <script src="{{asset('assets/js/moment.min.js')}}"></script>
         <script src="{{asset('assets/js/stisla.js')}}"></script>
 
         <!-- Template JS File -->
